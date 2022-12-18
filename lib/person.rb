12c59cbdbb6f674aebdadb93e9f5c1caca86cc81
Michael Davis <micdavis93@gmail.com>
2022-12-18 11:16:28 -0700

    commit tree 7d88949feeefe3102a97f730b13d9efa126edf74

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+# require pry
+
 class Person
-  # your code here
+
+  def initialize(attributes=nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+        # binding.pry
+      end
+    end
+  end
+
 end
+
+# bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+# bob = Person.new(bob_attributes)
+# puts bob.name
+# puts bob.hair_color
